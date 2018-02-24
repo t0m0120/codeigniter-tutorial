8ec28392e366b9eb0cc88749a6775f250822f1e6
t0m0120 <highwest.t@gmail.com>
2018-02-24 20:31:39 +0900

    indexは全ニュースの取得 viewはslug指定でのニュース取得

--- application/controllers/News.php
+++ application/controllers/News.php
@@ -5,4 +5,12 @@
 			$this->load->model('news_models');
 			$this->load->helper('url_helper');
 		}
+
+		public function index() {
+			$data['news'] = $this->news_model->get_news();
+		}
+
+		public function view($slug = NULL) {
+			$data[`news_item`] = $this->news_model->get_news($slug);
+		}
 	}
