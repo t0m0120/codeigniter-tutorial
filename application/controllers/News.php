f3d34b2534d15611da8715c40b1fed7096ba1028
t0m0120 <highwest.t@gmail.com>
2018-02-24 21:14:25 +0900

    slugから検索をかけ特定のニュース記事が帰ってくるようになる

--- application/controllers/News.php
+++ application/controllers/News.php
@@ -18,5 +18,16 @@
 
 		public function view($slug = NULL) {
 			$data['news_item'] = $this->news_model->get_news($slug);
+
+			if(empty($data['news_item'])) {
+				show_404();
+			}
+
+			$data['title'] = $data['news_item']['title'];
+
+			$this->load->view('templates/header', $data);
+			$this->load->view('news/view', $data);
+			$this->load->view('templates/footer', $data);
+
 		}
 	}
